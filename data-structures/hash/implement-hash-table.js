839810fa86e443baa75c4e837c44503344206410
muhammad-asghar-ali <masgharali.eng@gmail.com>
2024-01-13 22:45:57 +0500

    feat(): trying to implement hash table

---
+++ data-structures/hash/implement-hash-table.js
@@ -0,0 +1,85 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  } // O(1)
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  } // O(1)
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  } // O(n)
+
+  values() {
+    const valuesArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        valuesArray.push(this.data[i][0][1]);
+      }
+    }
+    return valuesArray;
+  } // O(n)
+
+  keysWithoutCollision() {
+    if (!this.data.length) {
+      return undefined;
+    }
+    let result = [];
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+      // if it's not an empty memory cell
+      if (this.data[i] && this.data[i].length) {
+        // but also loop through all the potential collisions
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0]);
+          }
+        } else {
+          result.push(this.data[i][0]);
+        }
+      }
+    }
+    return result;
+  }
+}
+
+const myHash = new HashTable(60);
+console.log(myHash.set('apple', 40));
+console.log(myHash.set('apples', 10));
+console.log(myHash.set('oranges', 20));
+console.log(myHash.get('apples'));
+console.log(myHash.keys());
+console.log(myHash.values());
